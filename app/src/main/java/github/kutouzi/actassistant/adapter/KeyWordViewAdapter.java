14a002b356b6d0e7d228ea9d85eb12ee3c277346
Kutouzi <qq454212450@gmail.com>
2024-05-29 18:46:09 +0900

    Add new futures

--- app/src/main/java/github/kutouzi/actassistant/adapter/KeyWordViewAdapter.java
+++ app/src/main/java/github/kutouzi/actassistant/adapter/KeyWordViewAdapter.java
@@ -9,11 +9,27 @@ import android.widget.ImageButton;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import java.util.List;
+import com.gsls.gt.GT;
 
+import java.util.List;
+import java.util.Optional;
+
+import github.kutouzi.actassistant.MainActivity;
 import github.kutouzi.actassistant.R;
+import github.kutouzi.actassistant.entity.KeyWordData;
+import github.kutouzi.actassistant.enums.JsonFileDefinition;
+import github.kutouzi.actassistant.enums.KeyWordListDefinition;
+import github.kutouzi.actassistant.io.JsonFileIO;
 
 public class KeyWordViewAdapter extends RecyclerView.Adapter<KeyWordViewAdapter.ViewHolder> {
+    public List<String> getKeyWordList() {
+        return keyWordList;
+    }
+
+    public void setKeyWordList(List<String> keyWordList) {
+        this.keyWordList = keyWordList;
+    }
+
     private List<String> keyWordList;
     public KeyWordViewAdapter(List<String> keyWordList) {
         this.keyWordList = keyWordList;
@@ -30,16 +46,46 @@ public class KeyWordViewAdapter extends RecyclerView.Adapter<KeyWordViewAdapter.
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.keyWordEditText.setText(keyWordList.get(position));
         holder.keyWordEditText.setOnFocusChangeListener((v, hasFocus) -> {
-            // TODO:关键词写入文件
+            if(!hasFocus){
+                keyWordList.set(position,holder.keyWordEditText.getText().toString());
+                updateJsonFile(v);
+            }
         });
         holder.removeButton.setOnClickListener(v -> {
-            // TODO:移除当前项
+            keyWordList.remove(position);
+            notifyItemRemoved(position);
         });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return keyWordList.size();
+    }
+
+    public void addItem(String s,View v){
+        keyWordList.add(s);
+        updateJsonFile(v);
+        // 添加到最尾部
+        notifyItemInserted(getItemCount());
+    }
+
+    private void updateJsonFile(View v){
+        KeyWordData keyWordData = JsonFileIO.readKeyWordDataJson(v.getContext(),JsonFileDefinition.KEYWORD_JSON_NAME);
+        Optional.ofNullable(keyWordData).ifPresent(s->{
+            if(MainActivity.listName.equals(KeyWordListDefinition.PINGDUODUO_CANCELABLE_KEYWORD_LIST)){
+                keyWordData.setPingduoduoCancelableKeyWordList(keyWordList);
+                JsonFileIO.writeKeyWordDataJson(v.getContext(), JsonFileDefinition.KEYWORD_JSON_NAME,keyWordData);
+            } else if (MainActivity.listName.equals(KeyWordListDefinition.PINGDUODUO_CLICKABLE_KEYWORD_LIST)) {
+                keyWordData.setPingduoduoClickableKeyWordList(keyWordList);
+                JsonFileIO.writeKeyWordDataJson(v.getContext(), JsonFileDefinition.KEYWORD_JSON_NAME,keyWordData);
+            } else if (MainActivity.listName.equals(KeyWordListDefinition.MEITUAN_CANCELABLE_KEYWORD_LIST)) {
+                keyWordData.setMeituanCancelableKeyWordList(keyWordList);
+                JsonFileIO.writeKeyWordDataJson(v.getContext(), JsonFileDefinition.KEYWORD_JSON_NAME,keyWordData);
+            } else if (MainActivity.listName.equals(KeyWordListDefinition.MEITUAN_CANCELABLE_KEYWORD_LIST)) {
+                keyWordData.setMeituanClickableKeyWordList(keyWordList);
+                JsonFileIO.writeKeyWordDataJson(v.getContext(), JsonFileDefinition.KEYWORD_JSON_NAME,keyWordData);
+            }
+        });
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
